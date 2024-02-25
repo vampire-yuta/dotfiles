051d880c9c6f6c1c82fa768612dc2d1a46cc3807
vampire-yuta <yuta@vamdemicsystem.black>
2024-02-25 17:00:53 +0900

    install.psついか

--- windows11/install.ps1
+++ windows11/install.ps1
@@ -5,3 +5,16 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 # Install Packages
 cd choco
 choco install -y package.config
+
+# Install KeyMapping
+Add-Content -Path $PROFILE.CurrentUserAllHosts -Value @"
+Set-PSReadlineKeyHandler -Key 'Ctrl+u' -Function BackwardDeleteLine
+Set-PSReadlineKeyHandler -Key 'Ctrl+b' -Function BackwardChar
+Set-PSReadlineKeyHandler -Key 'Ctrl+f' -Function ForwardChar
+Set-PSReadlineKeyHandler -Key 'Ctrl+d' -Function DeleteChar
+Set-PSReadlineKeyHandler -Key 'Ctrl+h' -Function BackwardDeleteChar
+Set-PSReadlineKeyHandler -Key 'Ctrl+p' -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key 'Ctrl+n' -Function HistorySearchForward
+Set-PSReadlineKeyHandler -Key 'Ctrl+a' -Function BeginningOfLine
+Set-PSReadlineKeyHandler -Key 'Ctrl+e' -Function EndOfLine
+"@
