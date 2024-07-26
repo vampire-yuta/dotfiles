84712078572289ce21dceb57f27ae7c652135d1d
vampire-yuta <yuta@vamdemicsystem.black>
2024-07-26 13:35:02 +0900

    profile.ps1

--- windows11/install_with_winget.ps1
+++ windows11/install_with_winget.ps1
@@ -2,15 +2,5 @@
 cd winget
 winget import winget.json
 
-# Install KeyMapping
-Add-Content -Path $PROFILE.CurrentUserAllHosts -Value @"
-Set-PSReadlineKeyHandler -Key 'Ctrl+u' -Function BackwardDeleteLine
-Set-PSReadlineKeyHandler -Key 'Ctrl+b' -Function BackwardChar
-Set-PSReadlineKeyHandler -Key 'Ctrl+f' -Function ForwardChar
-Set-PSReadlineKeyHandler -Key 'Ctrl+d' -Function DeleteChar
-Set-PSReadlineKeyHandler -Key 'Ctrl+h' -Function BackwardDeleteChar
-Set-PSReadlineKeyHandler -Key 'Ctrl+p' -Function HistorySearchBackward
-Set-PSReadlineKeyHandler -Key 'Ctrl+n' -Function HistorySearchForward
-Set-PSReadlineKeyHandler -Key 'Ctrl+a' -Function BeginningOfLine
-Set-PSReadlineKeyHandler -Key 'Ctrl+e' -Function EndOfLine
-"@
+# Put profilie.ps
+Copy-Item -Path profile.ps1 -Destination $HOME\Documents\PowerShell\profile.ps1 -Force
